6f65ddc829dfaefc50138f4bae81be0392b04f99
Akila Dilshan Edirisooriya <edirisooriyaakila@gmail.com>
2025-02-18 23:38:13 +0530

    Implement Driver Update functionality

--- app/DriverUpdate/page.jsx
+++ app/DriverUpdate/page.jsx
@@ -1,9 +1,10 @@
 "use client";
 import React, { useState } from "react";
 
-export default function page() {
+export default function DriverUpdate() {
   const [errorMsg, setErrorMsg] = useState(false);
   const [driverDetailsWindow, setDriverDetailsWindow] = useState(false);
+  const [updateMessage, setUpdateMessage] = useState("");
 
   const [driverId, setDriverId] = useState("");
 
@@ -14,53 +15,87 @@ export default function page() {
   const [nicText, setNicText] = useState("");
   const [registeredDateText, setRegisteredText] = useState("");
 
-  const handleSearch = async (e)=>{
+  const handleSearch = async (e) => {
     e.preventDefault();
-    
-    if(!driverId){
-        setErrorMsg("This field is required!");
-        return;
+    setDriverDetailsWindow(false);
+    setUpdateMessage("");
+    if (!driverId) {
+      setErrorMsg("This field is required!");      
+      return;
     }
 
-    try{
-        const request = await fetch(
-            `http://localhost:8080/api/v1/driver/driver-search?driverId=${encodeURIComponent(driverId)}`,
-            {
-                method: "GET"
-            }
-        );
-        
-        if(request.ok){
-            const response = await request.json();
-            if(response.message){
-                setErrorMsg(response.message);
-                setDriverDetailsWindow(false);
-            }else{
-                setDriverIdText(response.driverId);
-                setFirstNameText(response.firstName);
-                setLastNameText(response.lastName);
-                setContactText(response.contactNumber);
-                setNicText(response.nic);
-                setRegisteredText(response.registeredDate);
-
-                setDriverDetailsWindow(true);
-                setErrorMsg(false);
-            }
-        }else{
-            alert("Error in response. Please contact administrator!");
+    try {
+      const request = await fetch(
+        `http://localhost:8080/api/v1/driver/driver-search?driverId=${encodeURIComponent(
+          driverId
+        )}`,
+        {
+          method: "GET",
         }
+      );
 
-    }catch(error){
-        alert(error);
+      if (request.ok) {
+        const response = await request.json();
+        if (response.message) {
+          setErrorMsg(response.message);
+          setDriverDetailsWindow(false);
+        } else {
+          setDriverIdText(response.driverId);
+          setFirstNameText(response.firstName);
+          setLastNameText(response.lastName);
+          setContactText(response.contactNumber);
+          setNicText(response.nic);
+          setRegisteredText(response.registeredDate);
+
+          setDriverDetailsWindow(true);
+          setErrorMsg(false);
+        }
+      } else {
+        alert("Error in response. Please contact administrator!");
+      }
+    } catch (error) {
+      alert(error);
     }
-  }
+  };
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    try {
+      const request = await fetch(
+        "http://localhost:8080/api/v1/driver/driver-update",
+        {
+          method: "POST",
+          headers: {"Content-Type":"application/json"},
+          body: JSON.stringify( {
+            driverId: driverIdText,
+            firstName: firstNameText,
+            lastName: lastNameText,
+            contactNumber: contactText,
+            nic: nicText,
+            registeredDate: registeredDateText,
+          },
+        )
+        }
+      );
+      if (request.ok) {
+        const response = await request.text();
+        setUpdateMessage(response);
+      } else {
+        setUpdateMessage(
+          "No responses from server. Please contact administrator!"
+        );
+      }
+    } catch (error) {
+      setUpdateMessage(error);
+    }
+  };
 
   return (
     <div>
       <div className="flex-col items-center justify-center">
         <div className=" bg-rose-700 text-white min-w-screen h-[35px] flex items-center justify-center hover:bg-rose-600 shadow-md">
           <label className="text-white text-xl font-serif">
-            Driver Details
+            Update Driver Details
           </label>
         </div>
 
@@ -71,7 +106,9 @@ export default function page() {
               className="mt-2 text-md w-[300px] ring-1 ring-blue-500 ml-2 border-none rounded-sm hover:shadow-md px-2 h-[25px]"
               type="text"
               placeholder="Please provide Driver ID"
-              onChange={(e)=>{setDriverId(e.target.value)}}
+              onChange={(e) => {
+                setDriverId(e.target.value);
+              }}
             />
             <input
               className="mt-1 ml-4 h-[30px] w-[80px] bg-green-600 text-white font-serif rounded-md shadow-md hover:bg-green-500"
@@ -92,115 +129,128 @@ export default function page() {
       </div>
 
       <div>
-        {
-            driverDetailsWindow &&
-<form>
-          <div>
-            <div className="flex-col items-center justify-center mt-5">
-              <div className=" bg-blue-50 text-white min-w-screen h-[35px] flex items-center justify-center shadow-md">
-                <label className="text-blue-800 text-xl font-serif">
-                  Driver Details for provided Driver ID
-                </label>
-              </div>
-            </div>
-
-            <div className="mt-6 bg-white h-[150px] shadow-md hover:shadow-lg flex flex-col items-center justify-center">
-              <div className="flex flex-row">
-                <div className="flex flex-row ml-[43px]">
-                  <div>
-                    <label className="ml-4 text-blue-500">Driver ID:</label>
-                  </div>
-                  <div>
-                    <input
-                      className="text-md text-slate-600 w-[300px] ring-1 ring-blue-500 ml-2 border-none rounded-sm hover:shadow-md px-2"
-                      type="text"
-                      readOnly
-                      onChange={(e) => driverId(e.target.value)}
-                      value={driverIdText}
-                    />
-                  </div>
-                </div>
-
-                <div className="flex flex-row ml-[50px]">
-                  <div>
-                    <label className="ml-4 text-blue-500">First Name:</label>
-                  </div>
-                  <div>
-                    <input
-                      className="text-slate-600 text-md w-[300px] ring-1 ring-blue-500 ml-2 border-none rounded-sm hover:shadow-md px-2"
-                      type="text"
-                      readOnly
-                      value={firstNameText}
-                    />
-                  </div>
+        {driverDetailsWindow && (
+          <form>
+            <div>
+              <div className="flex-col items-center justify-center mt-5">
+                <div className=" bg-blue-50 text-white min-w-screen h-[35px] flex items-center justify-center shadow-md">
+                  <label className="text-blue-800 text-xl font-serif">
+                    Driver Details for provided Driver ID
+                  </label>
                 </div>
               </div>
 
-              <div className="flex flex-row mt-4">
-                <div className="flex flex-row ml-[28px]">
-                  <div>
-                    <label className="ml-4 text-blue-500">Last Name:</label>
-                  </div>
-                  <div>
-                    <input
-                      className="text-slate-600 text-md w-[300px] ring-1 ring-blue-500 ml-2 border-none rounded-sm hover:shadow-md px-2"
-                      type="text"
-                      readOnly
-                      value={lastNameText}
-                    />
-                  </div>
-                </div>
-
-                <div className="flex flex-row ml-[75px]">
-                  <div>
-                    <label className="ml-4 text-blue-500">Contact:</label>
-                  </div>
-                  <div>
-                    <input
-                      className="text-slate-600 text-md w-[300px] ring-1 ring-blue-500 ml-2 border-none rounded-sm hover:shadow-md px-2"
-                      type="text"
-                      readOnly
-                      value={contactText}
-                    />
-                  </div>
-                </div>
-              </div>
-
-              <div className="flex flex-row mt-4 ml-[78px]">
+              <div className="mt-6 bg-white h-[190px] shadow-md hover:shadow-lg">
                 <div className="flex flex-row">
-                  <div>
-                    <label className="ml-4 text-blue-500">NIC:</label>
+                  <div className="flex flex-row ml-[43px]">
+                    <div>
+                      <label className="ml-4 text-blue-500">Driver ID:</label>
+                    </div>
+                    <div>
+                      <input
+                        className="text-md text-slate-600 w-[300px] ring-1 ring-blue-500 ml-2 border-none rounded-sm hover:shadow-md px-2"
+                        type="text"
+                        readOnly
+                        onChange={(e) => driverId(e.target.value)}
+                        value={driverIdText}
+                      />
+                    </div>
                   </div>
-                  <div>
-                    <input
-                      className="text-slate-600 text-md w-[300px] ring-1 ring-blue-500 ml-2 border-none rounded-sm hover:shadow-md px-2"
-                      type="text"
-                      readOnly
-                      value={nicText}
-                    />
+
+                  <div className="flex flex-row ml-[50px]">
+                    <div>
+                      <label className="ml-4 text-blue-500">First Name:</label>
+                    </div>
+                    <div>
+                      <input
+                        onChange={(e) => setFirstNameText(e.target.value)}
+                        className="text-slate-600 text-md w-[300px] ring-1 ring-blue-500 ml-2 border-none rounded-sm hover:shadow-md px-2"
+                        type="text"
+                        value={firstNameText}
+                      />
+                    </div>
                   </div>
                 </div>
 
-                <div className="flex flex-row ml-[15px]">
-                  <div>
-                    <label className="ml-4 text-blue-500">
-                      Registered Date:
-                    </label>
+                <div className="flex flex-row mt-4">
+                  <div className="flex flex-row ml-[28px]">
+                    <div>
+                      <label className="ml-4 text-blue-500">Last Name:</label>
+                    </div>
+                    <div>
+                      <input
+                        onChange={(e) => setLastNameText(e.target.value)}
+                        className="text-slate-600 text-md w-[300px] ring-1 ring-blue-500 ml-2 border-none rounded-sm hover:shadow-md px-2"
+                        type="text"
+                        value={lastNameText}
+                      />
+                    </div>
                   </div>
-                  <div>
-                    <input
-                      className="text-slate-600 text-md w-[300px] ring-1 ring-blue-500 ml-2 border-none rounded-sm hover:shadow-md px-2"
-                      type="text"
-                      readOnly
-                      value={registeredDateText}
-                    />
+
+                  <div className="flex flex-row ml-[75px]">
+                    <div>
+                      <label className="ml-4 text-blue-500">Contact:</label>
+                    </div>
+                    <div>
+                      <input
+                        onChange={(e) => setContactText(e.target.value)}
+                        className="text-slate-600 text-md w-[300px] ring-1 ring-blue-500 ml-2 border-none rounded-sm hover:shadow-md px-2"
+                        type="text"
+                        value={contactText}
+                      />
+                    </div>
                   </div>
+                </div>
+
+                <div className="flex flex-row mt-4 ml-[78px]">
+                  <div className="flex flex-row">
+                    <div>
+                      <label className="ml-4 text-blue-500">NIC:</label>
+                    </div>
+                    <div>
+                      <input
+                        onChange={(e) => setNicText(e.target.value)}
+                        className="text-slate-600 text-md w-[300px] ring-1 ring-blue-500 ml-2 border-none rounded-sm hover:shadow-md px-2"
+                        type="text"
+                        value={nicText}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="flex flex-row ml-[15px]">
+                    <div>
+                      <label className="ml-4 text-blue-500">
+                        Registered Date:
+                      </label>
+                    </div>
+                    <div>
+                      <input
+                        className="text-slate-600 text-md w-[300px] ring-1 ring-blue-500 ml-2 border-none rounded-sm hover:shadow-md px-2"
+                        type="text"
+                        readOnly
+                        value={registeredDateText}
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex flex-col items-center justify-center">
+                  <input
+                  onClick={handleUpdate}
+                    className="mt-4 h-[30px] w-[200px] bg-blue-600 text-white font-serif rounded-md shadow-md hover:bg-blue-500"
+                    type="submit"
+                    value="Update"
+                  />
+                  {updateMessage && (
+                    <div className="text-red-600 mt-2">
+                      <label>{updateMessage}</label>
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
-          </div>
-        </form>
-        }        
+          </form>
+        )}
       </div>
     </div>
   );
